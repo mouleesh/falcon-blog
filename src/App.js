3279f22cbf3e8b16a8242303692680b6b52fd04c
Guru <guru@Gurus-MacBook-Pro.local>
2024-08-28 10:56:42 +0530

    Login box ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleSignIn = () => {
+      console.log("username is ", username)
+      console.log("Password is ", password)
+  }
+
+  const handleUserInputs = (event) => {
+    if(event.target.name === "user"){
+      setUsername(event.target.value)
+    } else if(event.target.name === "pass") {
+      setPassword(event.target.value)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='login-container'>
+      <h1 className='login-title'>Login</h1>
+      <div>
+        <label className='login-label'>Username: 
+          <input className='login-input' type="text" name="user" value={username} onChange={handleUserInputs}/>
+        </label>
+      </div>
+      
+      <div>
+        <label>Password: 
+          <input className='login-input' type="password" name="pass" value={password} onChange={handleUserInputs}/>
+          </label>
+      </div>
+      
+      <button className='login-button' onClick={handleSignIn}>Sign In</button>
     </div>
   );
 }
